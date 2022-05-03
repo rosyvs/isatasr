1db238db7c5167cebe3f4a337b6418fc27022bc9
rosyvs <rosy.southwell@gmail.com>
2022-05-03 17:26:09 -0600

    :sparkles: TAD_segmenter added

--- TAD_segmenter.py
+++ TAD_segmenter.py
@@ -1,59 +1,207 @@
-import shutil
-import sys
+import torchaudio
+from torch import tensor, cat, squeeze
 import os
+from pathlib import Path
 import re
-import subprocess
-import argparse
-from TAD_orig.tad_chunks import Frame, frame_generator, read_wave, write_wave
-
-# # if calling from terminal (see whw code):
-# parser = argparse.ArgumentParser(description='provide session list (.ctl file)')
-# parser.add_argument('ctl')
-# args = parser.parse_args()
-
-# for devel: 
-args_ctl = os.path.join('configs', 'one_sess.txt')
+import pandas as pd
+import numpy as np
+from pydub.audio_segment import AudioSegment, effects
+from speechbrain.pretrained import EncoderClassifier, SpeakerRecognition
+from speechbrain.utils.metric_stats import EER
+from rosy_asr_utils import get_sess_audio
 
 
+# ASR pipeline
+# 1. prepSessDirs
+# 2. VAD_segmenter / TAD_segmenter (uses automated segmentation) OR segFromAnnotation (uses timestamped transcript)
+# 3. ASRsegwiseGoogle (or REV, Watson)
+# 4. WER_by_speaker (after formatELANtranscripts/formatREVtranscripts)
 
-with open(args_ctl) as ctl:
-    sesslist = (line.rstrip() for line in ctl) 
-    sesslist = list(os.path.normpath(line) for line in sesslist if line)
+# Takes .wav audio from session directories with relative path <sesspath> specifed in control file
+# - uses TAD to segment audio into utterances, requires enrollment audio
+# - concatenates utterances to blocks of maximum duration blksecs (1 minute suggested as 
+# most economcial for sending to REV, max duration for synchronous Google ASR)
+# - exports segment and block audio in subdirectories (see below)
 
-for sesspath in sesslist: # TEMP DEBUG
-    print(f'sesspath: {sesspath}')
-    sesspath = sesspath.strip()
-    sessname = os.path.basename(sesspath)
-    tgtDir = os.path.join(sesspath,'targets')
-    segDir = os.path.join(sesspath,'segments')
+# session directory structure for a given audio file should be:
+#    |--{sessname}
+#       |--{sessname}.wav # full session audio
+# the following will be generated:
+#       |--TAD_{sessname}.blk # maps block and segment numbers to times in full session audio; columns block_no, segment_no, start_s, end_s
+#       |--TADblocks
+#          |--{sessname}_{block_no}.wav
+#       |--TADsegments
+#          |--{sessname}_{segment_no}.wav       
 
-    print(f'Attempting TAD for {sessname}...')
-    # check for target audio
-    if not os.path.exists(tgtDir):
-        print(' !! No target audio provided. Please provide a subfolder "targets" containing .wav files in the session directory')
-        continue
-
-    tgt_list = [f for f in os.listdir(tgtDir) if f.endswith('.wav')]
-    n_tgt = len(tgt_list)
-    if n_tgt ==0:
-        print(' !! No target audio provided. Please provide .wav files in the session\'s /targets/ subfolder')
-        continue
-    #     
-    for t in tgt_list:
-        print(f'    generating XVector for target {t}...')
-        # make MFCCs from target audio
-        # compute XVector embedding of target
-
-    
-    seg_list = [f for f in os.listdir(segDir) if f.endswith('.wav')]
-    for s in seg_list:
-
-        # make MFCCs from segment audio
-        # ? chunk to frames before or after MFCC? 
+def segFromTAD(filelist,
+        win_length=1500,
+        win_slide=250,
+        threshold,
+        speechbrain_dir='../speechbrain/',
+        model_type='ecapa',
+        enrollment_dir = 'enrollment',
+        export_seg_audio=False,
+        file_suffix=''):
 
 
-        for f in frames:
-        # compute XVector embedding of frame
-        # PLDA scoring 
+    # ~~~~SET SOME CONSTANTS~~~~ #
+    # TAD OPTIONS: TODO: decide if we want these as function args or just hard code
+    minseg = 500 # minimum active duration (ms) to trigger seg start
+    minsil = 500 # minimum inactive duration (ms) to trigger seg end
 
-            score = is_target(tgt_XVs, frame_XV)
+
+    # SEGMENT BLOCKING OPTIONS:
+    BLKSECS = 59 # Max Duration to block segments into. Note: Google ASR has refused some blocks if exactly 60 seconds 
+    SPLIT_OVERLAP = 1 # for long segments which require cutting, overlap by this duration in seconds to avoid word splitting
+    # AUDIO OPTIONS:
+    SAMPLE_WIDTH = 2 # bytes per sample, should usually be 2
+    SAMPLE_RATE = 16000 # for webrtcvad needs an srate of 8/16/32/48kHz
+    CHANNELS = 1 # mono
+
+    # initialise encoder and verifier
+    if model_type == 'ecapa':
+        encoder = EncoderClassifier.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb", 
+            savedir=f"{speechbrain_dir}/pretrained_models/spkrec-ecapa-voxceleb")
+        verifier = SpeakerRecognition.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb", 
+            savedir=f"{speechbrain_dir}/pretrained_models/spkrec-ecapa-voxceleb")
+    if model_type == 'xvect':
+        encoder = EncoderClassifier.from_hparams(source="speechbrain/spkrec-xvect-voxceleb", 
+            savedir=f"{speechbrain_dir}/pretrained_models/spkrec-xvect-voxceleb")
+        verifier = SpeakerRecognition.from_hparams(source=f"{speechbrain_dir}/pretrained_models/spkrec-xvect-voxcelebTEST", 
+            savedir=f"{speechbrain_dir}/pretrained_models/spkrec-xvect-voxceleb")
+
+    # loop over sessions
+    with open(filelist) as ctl:
+        sesslist = (line.rstrip() for line in ctl) 
+        sesslist = list(os.path.normpath(line) for line in sesslist if line)
+
+    for sesspath in sesslist: 
+        print(f'sesspath: {sesspath}')
+        sesspath = sesspath.strip()
+        sessname = os.path.basename(sesspath)
+        blkmapFile = os.path.join(sesspath, f'TAD_{sessname}.blk')
+
+        if export_seg_audio: 
+            print('Exporting segmented audio...')
+            segDir = os.path.join(sesspath, f'TADsegments{file_suffix}')
+            blkDir = os.path.join(sesspath, f'TADblocks{file_suffix}')
+            os.makedirs(segDir, exist_ok=True)
+            os.makedirs(blkDir, exist_ok=True)
+
+        # get session audio to filter
+        audiofile = get_sess_audio(sesspath)
+        if not audiofile:
+            print('!!! No audio file found! Skipping...')
+            continue
+        else:
+            aud_type = Path(audiofile).suffix
+            print(f'Input media type: {aud_type}')
+
+        # load session audio
+        sessAudio = AudioSegment.from_file(audiofile)
+        # TODO: convert to torch or jus read diretcly with torch
+
+        # # set sample rate and channels 
+        sessAudio = sessAudio.set_channels(CHANNELS).set_sample_width(SAMPLE_WIDTH).set_frame_rate(SAMPLE_RATE)
+
+        # get enrollment audio
+        enrollment_list = []
+        found_enrFiles = [f for f in os.listdir(os.path.join(sesspath,enrollment_dir)) \
+            if f.split('.')[-1] in ['MOV', 'mov', 'WAV', 'wav', 'mp4', 'mp3', 'm4a', 'aac', 'flac', 'alac', 'ogg']]
+        print(f'found {len(found_enrFiles)} enrollment audio files...')
+
+        target_embeddings = {}
+        for f in found_enrFiles:
+            enrFile = os.path.join(sesspath, enrollment_dir, f)
+            # TODO: load direct into torch or convert
+            enrAudio = AudioSegment.from_file(enrFile)
+            enrDur = enrAudio.duration_seconds
+            # convert as needed
+            enrAudio =enrAudio.set_channels(CHANNELS).set_sample_width(SAMPLE_WIDTH).set_frame_rate(SAMPLE_RATE)
+            # strip keyword parts of filename in crude attempt to get target name 
+            speaker =  os.path.basename(f)
+            speaker = re.sub('enrollment_simulated_','',speaker)
+            speaker = re.sub('enrollment_','',speaker)
+
+            print('Precomputing target embeddings...')
+            signal, fs = torchaudio.load(enrFile)
+            if not fs == 16000:
+                # fs must be 16000 
+                to16k = torchaudio.transforms.Resample(fs, 16000)
+                signal = to16k(signal)
+            target_embeddings[speaker] = encoder.encode_batch(signal)
+
+        # get input file info
+        metadata = torchaudio.info(sessAudio)
+        total_frames = metadata.num_frames  
+        WIN_SIZE = SAMPLE_RATE * win_length/1000 # in frames
+        last_win_st = total_frames - WIN_SIZE  # start frame for last window
+
+    #~~~~TAD FILTER~~~~#
+    state = 'o'   # indicates whether inside or outside segment
+    seg_sf = 0 # start frame of first sample in segment
+    seg_ef = 0 # start frame of last sample in segment
+    # use sliding window of WIN_SIZE frames to generate samples
+    # compare each sample to all targets
+    # generate start and end times for target segments
+    segments = []
+    for win_st in range(0,last_win_st,SHIFT_LEN):
+        # read next sample of WIN_SIZE frames
+        win,fs = torchaudio.load(args.wav, frame_offset=win_st, num_frames=WIN_SIZE)
+        xv_samp = encoder.encode_batch(win)
+
+        # compare sample to each target
+        scores = []
+        for t in target_embeddings:
+            score = verifier.similarity(target_embeddings[t], xv_samp)
+            scores.append(score.item())
+        frame_score = 0
+
+        # set sample score to 1 if any target score > threshold
+        for s in scores:
+            if s > threshold: frame_score = 1
+        #tim = '%f.1' % (FRAME_DUR * win_st)
+        #print(tim, frame_score)
+
+        # if inside segment
+        if state == 'i':
+            # if sample is target continue inside seg
+            if frame_score:
+                seg_ef = win_st
+                continue
+
+            # sample is not target
+            # if seg too short, discard
+            if (seg_ef - seg_sf) < minseg:
+                state = 'o'
+                continue
+
+            # if sil too short, don't terminate seg
+            if ((win_st - seg_ef) + WIN_SIZE) < minsil: continue
+
+            # terminate and save segment
+            # determine seg boundaries in secs
+            # start mid first seg, end mid last seg
+            seg_stsec = (seg_sf + (WIN_SIZE/2)) * FRAME_DUR
+            seg_edsec = (seg_ef + (WIN_SIZE/2)) * FRAME_DUR
+            seg = []
+            seg.append(seg_stsec)
+            seg.append(seg_edsec)
+            seg.append(1)
+            for s in scores:
+                seg.append(s)
+            segments.append(seg)
+            state = 'o'
+
+        # if outside segment
+        else:
+            # if target segment start new seg
+            if frame_score:
+                seg_sf = win_st
+                seg_ef = win_st
+                state = 'i'
+            # else continue outside
+
+    with open("out.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(segments)
